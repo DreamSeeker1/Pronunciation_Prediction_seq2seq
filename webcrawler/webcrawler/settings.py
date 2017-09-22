4a849f54c404c1dfdd7dbfb4fc0a223856e30c64
YY <Yue>
2017-09-21 22:42:36 -0700

    crwaled upto suharto

--- webcrawler/webcrawler/settings.py
+++ webcrawler/webcrawler/settings.py
@@ -19,15 +19,16 @@ NEWSPIDER_MODULE = 'webcrawler.spiders'
 #USER_AGENT = 'webcrawler (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 0.3
+DOWNLOAD_DELAY = 0.5
+
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -59,8 +60,8 @@ COOKIES_ENABLED = False
 DOWNLOADER_MIDDLEWARES = {
     'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
     'webcrawler.middlewares.RotateUserAgentMiddleware': 543,
-    #'scrapy.contrib.downloadermiddleware.httpproxy.HttpProxyMiddleware': 110,
-    #'webcrawler.middlewares.ProxyMiddleware': 100
+    # 'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 110,
+    # 'webcrawler.middlewares.ProxyMiddleware': 100
 }
 
 # Enable or disable extensions
