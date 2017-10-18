f2bd8155845d2a1fb768a06b55d397bb9f84a774
YY <Yue>
2017-10-17 18:44:38 -0700

    add docstring to ./tensor_seq/data.py

--- tensor_seq/data.py
+++ tensor_seq/data.py
@@ -15,6 +15,13 @@ for word, pron in zip(source_data, target_data):
 
 # map the character and pronunciation to idx
 def word2index(data):
+    """Map each character in the word list and special symbols to idx(int)
+    Args:
+        data: a list contains the words.
+    Returns:
+        idx2word: a dict map idx to character and symbol.
+        word2idx: a dict map idx to character and symbol.
+    """
     special_words = ['<EOS>', '<GO>', '<PAD>', '<UNK>']
     set_words = list(set([c for line in data for c in line] + special_words))
 
@@ -24,6 +31,13 @@ def word2index(data):
 
 
 def phon2index(data):
+    """Map each symbol in arpabet and special symbol to idx
+    Args:
+        data: a list contains the pronunciation sequence for each word.
+    Returns:
+        idx2word: a dict map each idx to a symbol.
+        word2idx: a dict map each symbol to an idx.
+    """
     special_words = ['<EOS>', '<GO>', '<PAD>', '<UNK>']
     set_words = list(
         set([pho for line in data for pho in line.split()] + special_words))
@@ -41,6 +55,7 @@ source_int = [[source_letter_to_int.get(letter, source_letter_to_int['<UNK>'])
 target_int = [[target_letter_to_int.get(letter, target_letter_to_int['<UNK>'])
                for letter in line.split()] + [target_letter_to_int['<EOS>']] for line in target_data]
 
+# save all the objects to a file.
 with open('data.pickle', 'w') as f:
     pickle.dump([source_int_to_letter, source_letter_to_int, target_int_to_letter, target_letter_to_int, source_int,
                  target_int, word_pron], f)
